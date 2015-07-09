99fbc75aeeb9cadf61a9d16ded274b096df7d57b
Sam White <sam@samw.io>
2015-07-09 16:49:54 +0100

    all models and tests finished and working

--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -1,9 +1,9 @@
 class Subreddit < ActiveRecord::Base
-
+	VALID_SRURL_REGEX = /\A[a-z]+\z/i
 	before_save { self.url = url.downcase }
 
-	validates(:name, presence: true, uniqueness: {case_sensitive: false})
-	validates(:url, presence: true, uniqueness: { case_sensitive: false})
+	validates(:name, presence: true, uniqueness: { case_sensitive: false })
+	validates(:url, presence: true, format: { with: VALID_SRURL_REGEX }, uniqueness: { case_sensitive: false })
 	validates(:desc, presence: true)
 
 	has_many :post
