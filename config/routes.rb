c2e4c62f35875d138b2f4e43341b8ed5306e6ab3
Sam White <sam@samw.io>
2015-07-09 19:14:24 +0100

    generated devise controllers, added email as a field to registrations controller, added routes for generated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks',
+  }
   get 'static_pages/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
