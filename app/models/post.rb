548edd3066d38a8641bb3152f6a149470fc625a8
Sam White <sam@samw.io>
2015-07-09 16:13:40 +0100

    post tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,15 @@ class Post < ActiveRecord::Base
 
 	validates(:title, presence: true, length: { maximum: 100 })
 
+	validates(:body, presence: true, if: "has_no_url?")
+
 	belongs_to :user
 
 	has_many :comment
+
+	def is_self_post?
+		self.url.blank?
+	end
+
+	alias_method :has_no_url?, :is_self_post?
 end
