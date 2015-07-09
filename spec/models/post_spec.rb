50924c3d804f8f9a83457649ee02163e37c4649e
Sam White <sam@samw.io>
2015-07-09 16:13:40 +0100

    post tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "When creating a valid post" do
+  	it "should succeed with a valid title" do 
+  		p = Post.new(title: "POST TITLE", url: "www.google.com")
+  		expect(p.valid?).to eq(true)
+  	end
+  end
+  context "When attempting to create an invalid post" do
+  	it "should fail with no title" do 
+  		p = Post.new(title: "", url: "www.google.com")
+  		expect(p.valid?).to eq(false)
+  	end
+  	it "should fail with neither a URL nor a body" do 
+  		p = Post.new(title: "trololol")
+  		expect(p.valid?).to eq(false)
+  	end
+  end
 end
