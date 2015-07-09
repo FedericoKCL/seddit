d72a3be6474b7fc070bd5277e8ee7971bef0c0ed
Sam White <sam@samw.io>
2015-07-09 15:57:07 +0100

    comment tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'When creating a comment' do
+  	it 'should accept valid data' do
+  		c = Comment.new(text: "Hello, World!")
+  		expect(c.valid?).to eq(true)
+  	end
+  	it 'should be rejected if blank' do
+  		c = Comment.new(text: "")
+  		expect(c.valid?).to eq(false)
+  	end
+  end
 end
