fed12998a7cc589c6240ae4243b54c1fc6a81c48
Sam White <sam@samw.io>
2015-07-14 16:08:59 +0100

    Initial user summary component completed

--- app/controllers/users/view_controller.rb
+++
@@ -1,8 +0,0 @@
-class Users::ViewController < ActionController::Base
-  include ApplicationHelper
-  #GET /u/:username
-  def view
-    @subreddits = subreddits
-    @user = User.find_by(username: params[:username])
-  end
-end
