08dd15584084064e9c8b3df6c63743edf49fa7e0
Federico Capaldo <federico.capaldo@yahoo.co.uk>
2015-07-14 19:03:16 +0100

    React PostComponent styled with css.

--- app/assets/javascripts/components/PostComponent.jsx
+++ app/assets/javascripts/components/PostComponent.jsx
@@ -5,10 +5,11 @@ var Timestamp = require("react-time");
 var PostComponent = React.createClass({displayName: 'Post Component',
   render: function() {
     return (
-      <div className="row post-head">
+      <div className="row post-head post-box">
         <p className="post-title"> { this.props.post.title } </p>
-        <p className="post-body"> { this.props.post.body } </p>
         <p className="creation-date">Created: <Timestamp value={this.props.post.created_at} titleFormat="YYYY/MM/DD HH:mm" relative /></p>
+        <hr width="95%" />
+        <p className="post-body"> { this.props.post.body } </p>
       </div>
     );
   }
