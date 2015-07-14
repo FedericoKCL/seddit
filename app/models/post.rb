6e5912366c3ede30d12e3a99484bc83edb554358
Sam White <sam@samw.io>
2015-07-14 16:08:59 +0100

    Initial user summary component completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
-  validates(:title, presence: true, length: { maximum: 100 })
+  validates(:title, presence: true,
+                    length: { maximum: 100 }
+           )
 
   validates(:body, presence: true, if: proc { |a| a.url.blank? })
 
--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -2,8 +2,15 @@ class Subreddit < ActiveRecord::Base
   VALID_SRURL_REGEX = /\A[a-z]+\z/i
   before_save { self.url = url.downcase }
 
-  validates(:name, presence: true, uniqueness: { case_sensitive: false })
-  validates(:url, presence: true, format: { with: VALID_SRURL_REGEX }, uniqueness: { case_sensitive: false })
+  validates(:name, presence: true,
+                   length: { maximum: 20 },
+                   uniqueness: { case_sensitive: false }
+           )
+  validates(:url, presence: true,
+                  length: { maximum: 20 },
+                  format: { with: VALID_SRURL_REGEX },
+                  uniqueness: { case_sensitive: false }
+           )
   validates(:desc, presence: true)
 
   has_many :post
